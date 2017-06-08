d510168c5440a3972aeeeb852fb44a16f131ec17
wwqgtxx <wwqgtxx@gmail.com>
2017-06-08 14:48:45 +0800

    use BitSet replace HashSet to make run faster

--- src/main/java/xyz/wwqgtxx/testcalculate/App.java
+++ src/main/java/xyz/wwqgtxx/testcalculate/App.java
@@ -14,15 +14,23 @@ import java.util.stream.IntStream;
 public class App {
     private static Logger logger = LogManager.getLogger(App.class);
 
-    public static boolean calculate_second_step(List<HashSet<Integer>> list) {
+    public static boolean calculate_second_step(List<BitSet> list) {
 //        logger.info(list); // like: [[1], [1, 2], [1, 2, 4], [3, 4], [2, 3, 4], [1, 3, 4, 5]]
-        for (List<HashSet<Integer>> item : Generator.combination(list).simple(2)) {
-            HashSet<Integer> set_1 = item.get(0);
-            HashSet<Integer> set_2 = item.get(1);
-            Set<Integer> union_set = Sets.union(set_1, set_2);
-            boolean is_union = list.contains(union_set);
-            Set<Integer> intersection_set = Sets.intersection(set_1, set_2);
-            boolean is_intersection = list.contains(intersection_set);
+        boolean is_union;
+        boolean is_intersection;
+        BitSet set_1;
+        BitSet set_2;
+        BitSet union_set;
+        BitSet intersection_set;
+        for (List<BitSet> item : Generator.combination(list).simple(2)) {
+            set_1 = item.get(0);
+            set_2 = item.get(1);
+            union_set = (BitSet)set_1.clone();
+            union_set.or(set_2);
+            intersection_set  = (BitSet)set_1.clone();
+            intersection_set.and(set_2);
+            is_union = list.contains(union_set);
+            is_intersection = list.contains(intersection_set);
             if (!is_union || !is_intersection) {
                 return false;
             }
@@ -35,14 +43,20 @@ public class App {
         for (int i = 1; i <= length; i++) {
             init_data_list.add(i);
         }
-        HashSet<Integer> empty_set = new HashSet<>();
-        HashSet<Integer> init_data_set = new HashSet<>(init_data_list);
-        List<HashSet<Integer>> first_step_data_list = Generator
+        BitSet empty_set = new BitSet();
+        BitSet init_data_set = new BitSet();
+        init_data_list.forEach(init_data_set::set);
+        List<BitSet> first_step_data_list = Generator
                 .subset(init_data_list)
                 .simple()
                 .stream()
                 .filter((item) -> !item.isEmpty() && !item.equals(init_data_list))
-                .map(HashSet<Integer>::new)
+                .map((value)->{
+                    BitSet bitSet = new BitSet();
+                    value.forEach(bitSet::set);
+                    return bitSet;
+                })
+//                .map(HashSet<Integer>::new)
                 .collect(Collectors.toList());
 //        first_step_data_list.forEach(System.out::println);
         logger.info("first_step_data_list.size=" + first_step_data_list.size());
@@ -76,15 +90,14 @@ public class App {
 
     public static void main(String[] args) {
         int length;
-        if (args.length >0){
+        if (args.length > 0) {
             length = Integer.parseInt(args[0]);
-        }
-        else{
+        } else {
             System.out.println("Please input the length:");
             Scanner input = new Scanner(System.in);
             length = input.nextInt();
         }
-        System.out.println("Start Calculate length="+length);
+        System.out.println("Start Calculate length=" + length);
         calculate(length);
 
     }
--- src/test/java/xyz/wwqgtxx/testcalculate/AppTest.java
+++ src/test/java/xyz/wwqgtxx/testcalculate/AppTest.java
@@ -3,20 +3,54 @@ package xyz.wwqgtxx.testcalculate;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest
-{
+public class AppTest {
+
+    @Test
+    public void testCalculate_second_step() throws Exception {
+        List<BitSet> list = new ArrayList<>();
+        BitSet bitSet;
+        bitSet = new BitSet();
+        bitSet.set(1);
+        list.add(bitSet);
+        bitSet = new BitSet();
+        bitSet.set(2);
+        list.add(bitSet);
+        bitSet = new BitSet();
+        bitSet.set(1);
+        bitSet.set(2);
+        list.add(bitSet);
+        bitSet = new BitSet();
+        bitSet.set(1);
+        bitSet.set(3);
+        list.add(bitSet);
+        bitSet = new BitSet();
+        bitSet.set(2);
+        bitSet.set(3);
+        list.add(bitSet);
+        bitSet = new BitSet();
+        bitSet.set(1);
+        bitSet.set(2);
+        bitSet.set(3);
+        list.add(bitSet);
+        assertEquals(App.calculate_second_step(list), false);
+    }
+
     @Test
     public void testCalculate3() throws Exception {
-        assertEquals(App.calculate(3),29);
+        assertEquals(App.calculate(3), 29);
     }
 
     @Test
     public void testCalculate4() throws Exception {
-        assertEquals(App.calculate(4),355);
+        assertEquals(App.calculate(4), 355);
     }
 }
